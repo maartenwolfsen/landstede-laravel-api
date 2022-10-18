e02a1ffdc56e55f5bfb78fddabd72e42756c3508
Maarten Wolfsen <maartenwolfsen@gmail.com>
2022-10-18 17:06:09 +0200

    Improve Seeder

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -2,12 +2,41 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use Illuminate\Container\Container;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+use Faker\Generator;
 
 class ProductsTableSeeder extends Seeder
 {
+    /**
+     * The current Faker instance.
+     *
+     * @var \Faker\Generator
+     */
+    protected $faker;
+
+    /**
+     * Create a new seeder instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->faker = $this->withFaker();
+    }
+
+    /**
+     * Get a new Faker instance.
+     *
+     * @return \Faker\Generator
+     */
+    protected function withFaker()
+    {
+        return Container::getInstance()->make(Generator::class);
+    }
+
     /**
      * Run the database seeds.
      *
@@ -17,10 +46,10 @@ class ProductsTableSeeder extends Seeder
     {
         for ($i = 0; $i < 50; $i++) {
             DB::table('products')->insert([
-                'title' => Str::random(10),
-                'body' => Str::random(100),
-                'image' => "https://www.arraymedical.com/wp-content/uploads/2018/12/product-image-placeholder.jpg",
-                'price' => 99.99
+                'title' => $this->faker->word(),
+                'body' => $this->faker->paragraph(3),
+                'image' => "https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                'price' => $this->faker->randomFloat(2, 1, 200)
             ]);
         }
     }
